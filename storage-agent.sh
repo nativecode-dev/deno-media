9dcb3966952399fa421181248c2383b33900ed8f
Mike Pham <reginfo@nativecode.com>
2020-06-28 01:32:57 -0400

    chore: update script

--- storage-agent.sh
+++ storage-agent.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
 export DEBUG=*
-pm2 save
-pm2 start --name storage-agent deno -- run -A -c tsconfig.json --unstable run.ts storage-agent
+deno run -A -c tsconfig.json --unstable run.ts storage-agent
