601f990a7313856091085ad6c237d2530a227633
Mike Pham <reginfo@nativecode.com>
2020-07-01 00:56:22 -0400

    chore: uipdates

--- cinemon/lib/Middlewares/LogMiddleware.ts
+++ cinemon/lib/Middlewares/LogMiddleware.ts
@@ -2,10 +2,6 @@ import { Alo, Dent } from '../../deps.ts'
 
 @Alo.Middleware(new RegExp('/(?!favicon)'))
 export class LogMiddleware implements Alo.MiddlewareTarget<any> {
-  constructor(@Alo.Inject(Dent.LoggerType) logger: Dent.Lincoln) {
-    console.log('logger', logger)
-  }
-
   onPreRequest(context: Alo.Context<any>) {}
 
   onPostRequest(context: Alo.Context<any>) {}
