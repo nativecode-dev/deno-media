5951d223f6776433cc19f296769824586b06e6bf
Mike Pham <reginfo@nativecode.com>
2020-07-04 19:35:50 -0400

    chore: update deps

--- rest-cinemon/deps.ts
+++ rest-cinemon/deps.ts
@@ -1,4 +1,4 @@
-export * as Connectors from 'https://raw.githubusercontent.com/nativecode-dev/deno-connectors/v0.0.0/mod.ts'
+export * as Connectors from 'https://raw.githubusercontent.com/nativecode-dev/deno-connectors/v0.0.1/mod.ts'
 export * as Dent from 'https://raw.githubusercontent.com/nativecode-dev/dent/v0.0.0//mod.ts'
 
 export * as Documents from '../documents/mod.ts'
