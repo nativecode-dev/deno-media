5f6bafef700291c5aa01e6d9d7853d8bac4dc662
Mike Pham <reginfo@nativecode.com>
2020-07-03 23:02:37 -0400

    chore: logging

--- cinemon/lib/Middlewares/LogMiddleware.ts
+++ cinemon/lib/Middlewares/LogMiddleware.ts
@@ -3,7 +3,7 @@ import { Alo, Dent } from '../../deps.ts'
 @Alo.Middleware(new RegExp('/(?!favicon)'))
 export class LogMiddleware implements Alo.MiddlewareTarget<any> {
   onPreRequest(context: Alo.Context<any>) {
-    console.log(context.request.url)
+    console.log(context.request.method, context.request.url, context.request.headers)
   }
 
   onPostRequest(context: Alo.Context<any>) {}
