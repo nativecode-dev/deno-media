65c8e400abbfc91ae327263b524e091cbe59c365
Mike Pham <reginfo@nativecode.com>
2020-07-04 01:56:33 -0400

    chore: version stamp

--- cinemon/deps.ts
+++ cinemon/deps.ts
@@ -1,6 +1,6 @@
 export * as Alo from 'https://deno.land/x/alosaur@v0.19.4/mod.ts'
-export * as Connectors from 'https://raw.githubusercontent.com/nativecode-dev/deno-connectors/master/mod.ts'
-export * as Dent from 'https://raw.githubusercontent.com/nativecode-dev/dent/master/mod.ts'
+export * as Connectors from 'https://raw.githubusercontent.com/nativecode-dev/deno-connectors/v0.0.0/mod.ts'
+export * as Dent from 'https://raw.githubusercontent.com/nativecode-dev/dent/v0.0.0/mod.ts'
 
 export * as Documents from '../documents/mod.ts'
 export * as Messages from '../messages/mod.ts'
--- storage-agent/deps.ts
+++ storage-agent/deps.ts
@@ -1,8 +1,8 @@
 export * as Alo from 'https://deno.land/x/alosaur@v0.19.4/mod.ts'
 export * as Checksum from 'https://deno.land/std@0.59.0/hash/mod.ts'
-export * as Dent from 'https://raw.githubusercontent.com/nativecode-dev/dent/master/mod.ts'
+export * as Dent from 'https://raw.githubusercontent.com/nativecode-dev/dent/v0.0.0/mod.ts'
 export * as Path from 'https://deno.land/std@0.59.0/path/mod.ts'
-export * as RxJs from 'https://raw.githubusercontent.com/DenoBRComunitty/rxjs/master/mod.ts'
+export * as RxJs from 'https://raw.githubusercontent.com/DenoBRComunitty/rxjs/v0.0.0/mod.ts'
 
 export * as Documents from '../documents/mod.ts'
 export * as Messages from '../messages/mod.ts'
@@ -10,7 +10,7 @@ export * as Queues from '../queues/mod.ts'
 
 export { BError } from 'https://deno.land/x/berror/berror.ts'
 export { exists } from 'https://deno.land/std@0.59.0/fs/exists.ts'
-export { Scheduler, ScheduleType } from 'https://raw.githubusercontent.com/nativecode-dev/dent/master/scheduler/mod.ts'
+export { Scheduler, ScheduleType } from 'https://raw.githubusercontent.com/nativecode-dev/dent/v0.0.0/scheduler/mod.ts'
 
 export { CinemonClient } from '../rest-cinemon/mod.ts'
 export { retry, retryAsync } from 'https://deno.land/x/retry@v0.1.2/mod.ts'
